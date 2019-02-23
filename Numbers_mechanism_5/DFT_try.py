012bf07b96598ae61b1cc3ea759fb8684cbd438a
manuaatitya <ermanuaatitya@gmail.com>
2019-02-23 14:52:32 +0530

    added input file to the code

--- Numbers_mechanism_5/DFT_try.py
+++ Numbers_mechanism_5/DFT_try.py
@@ -2,16 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-n = int(input('Enter the number of points for which the curve has to be drawn\t'))
-x,y = [],[]
+# n = int(input('Enter the number of points for which the curve has to be drawn\t')) 
+n = 12
+x,y,b = [0,6,6,0,0,6.5,6.5,0.5,0.5,6.5,6.5,0],[0,0,6,6,10,10,9.5,9.5,6.5,6.5,-.5,-.5],[]
 
+'''
 for i in range(n):
 	ip = list(map(float,input('Enter the x and y co-rdinates separated by a space :\t').split()))
 	x.append(ip[0]),y.append(ip[1])
+'''
 	
 a = np.fft.fft(np.array(x) + 1j * np.array(y))
 freqs = np.fft.fftfreq(n)
 print(a),print(freqs)
+
+for i in a:
+	b.append(abs(i))
+	
+print(b)
 
 t = np.linspace(0.0, 2 * np.pi, 1000)
 vals = np.sum(a / n * np.exp(1j * np.multiply.outer(t, n * freqs)), axis=1)
